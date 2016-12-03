3b4e193e804838c4fc5e4279d0f7af0fc49aba03
Jean Champémont <git@jeanchampemont.com>
2016-12-03 21:50:30 +0100

    add travis publishing javadoc

---
+++ publish-javadoc.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ "$TRAVIS_REPO_SLUG" == "jchampemont/jabba-framework" ] && [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+
+  echo -e "Publishing javadoc...\n"
+
+  cp -R $1/build/docs/javadoc $HOME/$1-javadoc-latest
+
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/jchampemont/javadoc gh-pages > /dev/null
+
+  mkdir -p gh-pages/$1
+  cd gh-pages/$1
+  cp -Rf $HOME/$1-javadoc-latest .
+  if [ -z "$TRAVIS_TAG" ]; then
+    git rm -rf $TRAVIS_BRANCH
+    mv $1-javadoc-latest $TRAVIS_BRANCH
+  else
+    git rm -rf $TRAVIS_TAG
+    mv $1-javadoc-latest $TRAVIS_TAG
+  fi
+  git add -f .
+  git commit -m "Latest javadoc for $1 on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+  git push -fq origin gh-pages > /dev/null
+
+  echo -e "Published Javadoc to gh-pages.\n"
+
+fi
