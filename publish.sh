6d89a97b39ee1756412d4ec005a69e97e4a75475
Jean Champémont <git@jeanchampemont.com>
2016-12-04 10:19:41 +0100

    add variables and comments in publish.sh

--- publish.sh
+++ publish.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-if [ "$TRAVIS_REPO_SLUG" == "jchampemont/jabba-framework" ] && [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+# This script is used by travis-ci to publish javadoc and versions artifacts
+
+REPO_SLUG="jchampemont/jabba-framework"
+MVN_REPO_SLUG="jchampemont/maven-repository"
+JAVADOC_REPO_SLUG="jchampemont/javadoc"
+
+if [ "$TRAVIS_REPO_SLUG" == "$REPO_SLUG" ] && [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
 
   if [ -n "$TRAVIS_TAG" ]; then
     echo -e "Publishing maven artifacts...\n"
     ./gradlew -PghToken=$GH_TOKEN publish
-    cd $HOME/.gitRepos/jchampemont/maven-repository
+    cd $HOME/.gitRepos/$MVN_REPO_SLUG
     git add .
     git commit -m "Publish artifacts of $1 $TRAVIS_TAG on sucessful travis build $TRAVIS_BUILD_NUMBER"
+    git push
     cd -
   fi
 
@@ -18,7 +25,7 @@ if [ "$TRAVIS_REPO_SLUG" == "jchampemont/jabba-framework" ] && [ "$TRAVIS_JDK_VE
   cp -R $1/build/docs/javadoc $HOME/$1-javadoc-latest
 
   cd $HOME
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/jchampemont/javadoc gh-pages > /dev/null
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/$JAVADOC_REPO_SLUG gh-pages > /dev/null
 
   mkdir -p gh-pages/$1
   cd gh-pages/$1
